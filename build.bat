25353cf9fb3e1cad843a9d39a5780210e236469a
Found4k <88767692+Found4k@users.noreply.github.com>
2021-08-12 20:19:00 +1000

    Add files via upload

---
+++ build.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Welcome to the build (Build written by Found4k)
+PAUSE
+@echo off
+echo To build open the app with Gdevelop
+PAUSE
+@echo off
+echo Click the button next to play that looks like a paper
+PAUSE
+@echo off
+echo Then select the way you want to build it
+PAUSE
+@echo off
+echo and then it will do the rest
